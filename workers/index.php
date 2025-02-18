32c3f32d111b9e7896671540a9e349e4934e1540
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-18 08:56:54 -0500

    02-18 Workers 08:56

---
+++ workers/index.php
@@ -0,0 +1,208 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+if (!isset($_SESSION['nombre'])) {
+    header('Location: ../index.php');
+    exit();
+}
+// include __DIR__.'/../src/nav.php';
+require_once __DIR__.'/../src/gestion.php';
+$mod='repDiar';
+
+
+$ya = new DateTime();
+// $estados=opc_arr([['v' => 'SI', 'l' => 'SI'], ['v' => 'NO', 'l' => 'NO']],'NO');
+$usu=$_SESSION['documento'];
+$sql="SELECT id_usuario, nombre FROM `usuarios` WHERE (EXISTS (SELECT id_usuario, nombre FROM `usuarios`  WHERE  id_usuario =".$usu." AND perfil IN ('ADM') AND estado = 'A') OR id_usuario =".$usu.") and estado='A' ORDER BY 2";
+$colaborador=opc_sql($sql,$usu);
+// var_dump($colaborador);
+// var_dump($sql);
+$hoy = date("Y-m-d");
+$ayer = date("Y-m-d",strtotime($hoy."- 2 days"));
+$acc=acceBtns('repDiar');
+$btns='<button class="act-btn" data-mod='.$mod.' title="Actualizar"><i class="fas fa-rotate"></i></button>';
+if (isset($acc['crear']) && $acc['crear'] == 'SI') {
+    $btns .= '<button class="add-btn" data-mod='.$mod.' title="Nuevo"><i class="fas fa-plus"></i></button>';
+}
+if (isset($acc['importar']) && $acc['importar'] == 'SI') {
+    $btns .= '<button id="openModal" class="upload-btn" data-mod='.$mod.' title="Importar"><i class="fas fa-upload"></i></button>';
+}
+$perfi=datos_mysql("SELECT perfil as perfil FROM usuarios WHERE id_usuario='{$_SESSION['documento']}'");
+$perfil = (!$perfi['responseResult']) ? '' : $perfi['responseResult'][0]['perfil'];
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> Reporte Diario || <?php echo APP; ?></title>
+    <link href="../libs/css/menu.css?v=2.0" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+	<link rel="stylesheet" href="../libs/css/app.css?v=20.0">
+	<script src="../libs/js/main.js?v=14.0"></script>
+	<link rel="stylesheet" href="../libs/css/choices.min.css?v=2.0">
+    <script src="../libs/js/choices.min.js"></script>
+    <script src="../../libs/js/menu.js?v=1.0"></script>
+	<script>
+		let mod = 'repDiar';
+		let ruta_app = 'lib.php';
+		function actualizar() {
+			act_lista(mod);
+			badgeFilter(mod);
+		}
+	</script>
+    <?php include __DIR__.'/../src/nav.php'; ?>
+</head>
+<body Onload="actualizar();">
+	<div class="wrapper main" id='<?php echo $mod; ?>-main'>
+	<form method='post' id='fapp' onsubmit="return false;">
+	<!-- <input type="hidden" name="csrf_tkn" value="15266546545645454"> -->
+	<input type="hidden" name="csrf_tkn" value="<?php echo $_SESSION['csrf_tkn'];?>">
+		<div class="top-menu">
+			<input type="radio" name="slider"  id="filtros">
+			<input type="radio" name="slider" checked id="datos">
+			<nav>
+				<label for="filtros" class="filtros"><i class="fa-solid fa-sliders fa-rotate-90"></i>Filtros
+				<span class="badge badge-pill badge-warning" id='fil-badge'></span></label>
+				<label for="datos" class="datos"><i class="fas fa-table"></i>Datos</label>
+				<div class="slider"></div>
+			</nav>
+			<section>
+				<div class="content content-1">
+					<div class="title txt-center"><h2>Filtros</h2></div>
+					<div class="frm-filter poppins-font" id='<?php echo $mod; ?>-fil'>
+						<div class="input-box">
+							<label for="choices-multiple-remove-button">Colaborador :</label>
+                			<select class='choices-multiple-remove-button' id="fcol" name="fcol" multiple OnChange="actualizar();">
+								 <?php echo $colaborador; ?>
+                			</select>
+    					</div>
+						<div class="input-box">
+							<label for="fdes">Fecha Desde</label>
+    					    <input  type="date" class='captura' id="fdes" name="fdes" value='<?php echo $ayer; ?>' OnChange="actualizar();">
+    					</div>
+						<div class="input-box">
+						<label for="fhas">Fecha Hasta</label>
+    					    <input  type="date" class='captura' id="fhas" name="fhas" value='<?php echo $hoy; ?>' OnChange="actualizar();">
+    					</div>
+						<!-- <button  class="btn" OnClick="creaBtns('<?php /* echo $mod;  */?>');">Aplicar</button> -->
+					</div>
+					<div class='load'id='loader' z-index='0'></div>
+				</div>
+				<div class="content content-2">
+					<div class="title txt-center"><h2>REPORTE DIARIO</h2></div>
+					<div id='<?php echo $mod; ?>-btns' class="header">
+					<?php echo $btns ?>
+        			</div>
+					<div class='panel' id='<?php echo $mod; ?>'>
+						<span class='mensaje' id='<?php echo $mod; ?>-msj'></span>
+						<div class='contenido' id='<?php echo $mod; ?>-lis'></div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<div id='<?php echo $mod; ?>-frmcap'>
+		</div>
+		<div id='hallaz-frmcap'>
+		</div>
+		</form>
+    </div>
+	</div>
+		<div class="overlay" id="overlay" style=" visibility:hidden;" onClick="closeModal();">
+			<div class="toast" id="loader">
+				<div class="toast-content">
+				    <i class=""></i>
+				   	<div class='message' id='<?php echo $mod; ?>-toast'>	   
+						<span class="text text-1"></span>
+						<span class="text text-2"></span>
+					</div>
+				</div>
+				<i class="fa-solid fa-xmark close"></i>
+				<div class="progress"></div>
+			</div>
+		</div>
+		<div id="modal" class="modal">
+        	<div class="modal-content">
+        	    <span class="modal-close" id="closeModal">&times;</span>
+        	    <h2>Cargar Registros</h2>
+        	    <p>Por favor, seleccione un archivo CSV para cargar a la base de datos.</p>
+        	    <div class="file-upload">
+        	        <input type="file" id="fileInput" accept=".csv" />
+        	        <i class="fa-solid fa-cloud-arrow-up cloud-icon"></i>
+        	        <p id="file-name">Selecciona un archivo aquí</p>
+        	        <button type="button" class="browse-btn" onclick="document.getElementById('fileInput').click();">
+        	            Examinar
+        	        </button>
+        	    </div>
+        	    <div class="progress-container">
+				<div id="progressBar" class="progress-bar"></div>
+        	    </div>
+        	    <p id="progressText">0% completado</p>
+        	    <p id="statusMessage"></p>
+        	    <div class="button-container">
+        	        <button id="startLoading">Iniciar Carga</button>
+        	        <button id="cancelLoading" style="display: none;">Cancelar</button>
+        	        <button id="closeModal" style="display: none;">Cerrar</button>
+        	    </div>
+        	</div>
+    	</div>
+	<script>
+		document.addEventListener('DOMContentLoaded', () => {
+			const csrfInput = document.querySelector('input[name="csrf_tkn"]');
+    		window.csrfToken = csrfInput ? csrfInput.value : '';
+        	const fileInput = document.getElementById('fileInput'),
+        		startLoadingBtn = document.getElementById('startLoading');
+			startLoadingBtn.onclick = async () => {
+    			const file = fileInput.files[0];
+    			if (file) {
+        			try {
+        	    		// const userData = await getJSON('imp', 'deriva', 0, 'lib.php');
+        	    		error.log(userData);
+        	    		// Luego llamas a startImport con los parámetros adecuados
+        	    		//startImport(file, b, c, d);
+        			} catch (error) {
+        			    error.error('Error al obtener los datos: ', error);
+        			    statusMessage.textContent = 'Error al procesar la solicitud.';
+        			}
+    			} else {
+        			statusMessage.textContent = 'Por favor seleccione un archivo CSV.';
+    			}
+			};
+		});
+        function creaBtns(a) {
+		// Datos que se envían al servidor (si los necesitas)
+			const data = 'a=btn&tb='+a; 
+		// Llamada a pFetch con callback para crear botones
+			pFetch('lib.php', data, (responseData) => {
+			// Asumiendo que responseData contiene los permisos
+				if (responseData) {
+					crearBotones(responseData,a);
+				}
+			});
+		}
+	function grabar(tb = '', ev) {
+    if (tb === '' && ev.target.classList.contains(proc)) tb = proc;
+    const fields = document.getElementsByClassName('valido ' + tb);
+    for (let i = 0; i < fields.length; i++) {
+        if (!valido(fields[i])) {
+            fields[i].focus();
+            ev.preventDefault();
+            return;
+        }
+    }
+    const res = confirm("Desea guardar la información? Recuerda que no se podrá editar posteriormente.");
+    if (res) {
+        myFetch(ruta_app, `a=gra&tb=${tb}`)
+            .then(rta => {
+                handleResponse(rta);
+                if (rta && typeof rta === 'object' && rta.status !== 'error') {
+                    act_lista(tb);
+                }
+            })
+            .catch(error => {
+                console.error("Error en la petición:", error);
+                enqueueMessage('error', "Error en la petición.", 7000);
+            });
+    }
+}
+</script>
+</body>
---
+++ workers/lib.php
@@ -0,0 +1,153 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../src/gestion.php';
+$perf = perfil($_POST['tb']);
+if (!isset($_SESSION['documento'])) {
+    log_error("Error 20: Usuario No Autorizado.".$_SESSION['documento']);
+    http_response_code(401);
+    echo json_encode(['redirect' => '/']);
+    exit();
+}
+if (!isset($_POST['csrf_tkn']) || $_POST['csrf_tkn'] !== $_SESSION['csrf_tkn']) {
+    log_error("Error 24: Intento de CSRF detectado. " . $_POST['csrf_tkn'] . ' frente a ' . $_SESSION['csrf_tkn']);
+    http_response_code(403); // Prohibido
+    exit();
+}
+// Regenerar el token después de validarlo
+$a = filter_var($_POST['a'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$tb = filter_var($_POST['tb'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$func = $a . '_' . $tb;
+if (!function_exists($func)) {
+    log_error("Error 21: Función no encontrada. Intento de llamar a: " . $func);
+    http_response_code(400);
+    echo json_encode(['error' => 'Función no encontrada', 'funcion' => $func]);
+    exit();
+}
+try {
+    $rta = $func();
+    if (is_array($rta)) {
+        echo json_encode($rta);
+    } else {
+        echo $rta;
+    }
+} catch (Exception $e) {
+    log_error("Error 23: Excepción al ejecutar la función. Función: " . $func . ", Error: " . $e->getMessage());
+    http_response_code(500);
+    echo json_encode(['error' => 'Error interno del servidor']);
+}
+
+function whe_repDiar() {
+    $filtros = [];
+    $perfil = obtenerPerfil($_SESSION['documento']);
+    if (!empty($_POST['fcol']) && $perfil == 'ADM') {
+        $filtros[] = ['campo' => 'R.usu_create', 'valor' => limpiar_y_escapar_array(explode(",", $_POST['fcol'])), 'operador' => 'IN'];
+    } else {
+        $filtros[] = ['campo' => 'R.usu_create', 'valor' => limpiar_y_escapar_array([$_SESSION['documento']]), 'operador' => 'IN'];
+    }
+    if (!empty($_POST['fdes']) && !empty($_POST['fhas'])) {
+        $filtros[] = ['campo' => 'fecha_report', 'valor' => $_POST['fdes'], 'operador' => '>='];
+        $filtros[] = ['campo' => 'fecha_report', 'valor' => $_POST['fhas'], 'operador' => '<='];
+    }
+    return fil_where($filtros);
+}
+function lis_repDiar() {
+    $regxPag = 15;
+    $pag = si_noexiste('pag-repDiar', 1);
+    $offset = ($pag - 1) * $regxPag;$filter = whe_repDiar();$where = $filter['where'];$params = $filter['params'];$types = $filter['types'];
+    $tabla = "repor_diario";
+	$sqltot="SELECT COUNT(*) total  FROM `repor_diario` R LEFT JOIN nove_repdiar N ON R.id_rep = N.idrep LEFT JOIN usuarios U ON R.usu_create = U.id_usuario WHERE " . $where;
+    $total = obtener_total_registros($sqltot,$params, $types);
+    $sql = "SELECT R.`id_rep` AS ACCIONES, R.id_rep AS Cod_Registro, R.`fecha_report` AS Fecha_Reporte, R.`cant_report` AS Registros_Reportados, COUNT(N.idrep) AS Registros_Digitados, U.`nombre` AS Colaborador, R.`fecha_create` AS Fecha_Creacion 
+ FROM `repor_diario` R LEFT JOIN nove_repdiar N ON R.id_rep = N.idrep LEFT JOIN usuarios U ON R.usu_create = U.id_usuario";
+$where.=" GROUP BY R.id_rep, R.fecha_report, R.cant_report, U.nombre, R.fecha_create";
+    $datos = obtener_datos_paginados($sql, $where, $params, $types, $offset, $regxPag);
+	// show_sql($sql." WHERE ".$where. " LIMIT ?,?",array_merge($params,[$offset,$regxPag]),$types ."ii");
+     if ($datos === []) return no_reg();
+    return create_table($total, $datos, "repDiar", $regxPag, "lib.php");
+}
+ function focus_repDiar(){
+	return 'repDiar';
+   }
+   function men_repDiar(){
+	$rta=cap_menus('repDiar','pro');
+	return $rta;
+   }
+   function cap_menus($a,$b='cap',$con='con') {
+	$rta = "";
+	// $rta .= "<li class='fa-solid fa-floppy-disk $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
+	$rta .= "<button class='frm-btn $a grabar' onclick=\"grabar('$a', this);\"><span class='frm-txt'>Grabar</span><i class='fa-solid fa-floppy-disk icon'></i></button>";
+	/* $rta .="<button class='frm-btn $a actualizar' onclick=\"act_lista('".$a."',this);\"'>Actualizar</button>";
+	$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('".$a."',this);\"></li>"; */
+	return $rta;
+  }
+  function cmp_repDiar(){
+	$rta="";
+	$t=['id_rep'=>'','fecha_report'=>'','cant_report'=>''];
+	$w='repDiar';
+	$uPd = $_REQUEST['id']=='0' ? true : false;
+	$d=get_repDiar(); 
+	// print_r($d);
+	if ($d=="") {$d=$t;}
+	$o='docder';
+	// var_dump($_POST);
+	$c[]=new cmp('id','h',100,$d['id_rep'],$w,'',0,'','','',false,'','col-1');
+	$c[]=new cmp('fec','d',10,$d['fecha_report'],$w.' '.$o,'Fecha Reporte','','','',true,$uPd,'','col-2',"validDate(this,-3,0);");
+	$c[]=new cmp('can','n',9,$d['cant_report'],$w.' '.$o,'Cantidad Registros','','','',true,true,'','col-2');
+	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
+	$rta.="</div>";
+	return $rta;
+	}
+    function get_repDiar(){
+		if($_POST['id']=='0'){
+			return "";
+		}else{
+			$id=divide($_POST['id']);
+			$sql="SELECT * FROM repor_diario WHERE id_rep='".$id[0]."'";
+			$info=datos_mysql($sql);
+			return $info['responseResult'][0];		
+		} 
+	}
+    function gra_repDiar(){
+		$id=divide($_POST['id']);
+			if (empty($id[0])) { //verifica si el id no esta vacio para realizar un update o un insert
+                $sql = "INSERT INTO repor_diario VALUES(NULL,?,?,?,DATE_SUB(NOW(),INTERVAL 5 HOUR),NULL,NULL,'A')";
+                $params = [
+                    ['type' => 's', 'value' => $_POST['fec']],
+                    ['type' => 'i', 'value' => $_POST['can']],
+                    ['type' => 'i', 'value' => $_SESSION['documento']]
+                ];
+                // $types = "sii"; 
+			}else{
+                $sql = "UPDATE repor_diario SET cant_report = ? WHERE id_rep = ?";
+                $params = [
+                    ['type' => 'i', 'value' => $_POST['can']],
+                    ['type' => 'i', 'value' => $id[0]]
+                ];
+                // $types = "ii"; 
+            }
+/*             $param_values = array_map(fn($p) => $p['value'], $params);
+            $debug_sql = show_sql($sql, $param_values, $types);
+            $rta = ['status' => 'success', 'message' => $debug_sql]; */
+		$rta = mysql_prepd($sql, $params);
+		header('Content-Type: application/json; charset=utf-8'); 
+		echo json_encode($rta);
+        // $_SESSION['csrf_tkn'] = bin2hex(random_bytes(32));
+		exit;
+	}
+function formato_dato($a,$b,$c,$d){
+	$b=strtolower($b);
+	$rta=$c[$d];
+	if (($a=='repDiar') && ($b=='acciones')){
+		   $rta="<nav class='menu right'>";
+		   $rta.="<li class='fa-solid fa-pen-to-square icon' title='Editar Reporte Diario' id='".$c['ACCIONES']."' Onclick=\"mostrar('repDiar','pro',event,'','lib.php',4);\"></li>";
+           $rta.="<li class='fa-solid fa-triangle-exclamation icon' title='Hallazgos' id='".$c['ACCIONES']."' Onclick=\"mostrar('hallaz','pro',event,'','hallazgos.php',4,'Hallazgos');\"></li>";
+		   $rta.="</nav>";
+	   }    
+	return $rta;
+   }
+function bgcolor($a,$c,$f='c'){
+	$rta="";
+	//~ if ($a=='transacciones'&&$c['ESTADO']=='A') $rta='green';
+	return $rta;
+   }
+   ?>
