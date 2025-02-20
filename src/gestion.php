5b61e95042b5bfeca71dc817204bb82aeace66ae
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-20 14:18:13 -0500

    02-20 Workers 14:18

--- src/gestion.php
+++ src/gestion.php
@@ -359,8 +359,8 @@ function exec_sql($sql, $params = [], $types = "", $fetch_all = true) {
       } 
   } catch (mysqli_sql_exception $e) {
       log_error("exec_sql: Excepción en la consulta: " . $e->getMessage());
-      echo json_encode($e->getMessage());
-      // return null;
+      echo json_encode(['Error:'.$e->getMessage()]);
+      return null;
   } finally {
       if ($result) {
           $result->free_result(); // Liberar $result primero
