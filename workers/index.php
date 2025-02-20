6f5eb13e1167df7a30896ce86a5c681c4eedb2d0
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-20 13:31:54 -0500

    02-20 Workers 13:31

--- workers/index.php
+++ workers/index.php
@@ -1,9 +1,8 @@
 <?php
+require_once __DIR__ . '/../config/config.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__ . '/../config/config.php';
-ini_set('display_errors','1');
 if (!isset($_SESSION['nombre'])) {
     header('Location: ../index.php');
     exit();
