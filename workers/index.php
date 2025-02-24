26b85c259ab6b569298e298a98d564c3d0077a8c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-24 16:37:14 -0500

    02-24 Workers 16:37

--- workers/index.php
+++ workers/index.php
@@ -26,7 +26,7 @@ if (isset($acc['importar']) && $acc['importar'] == 'SI') {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> Empleados || <?php echo APP; ?></title>
                     <link href="../libs/css/menu.css?v=13.0" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="../libs/css/app.css?v=6.0">
+	<link rel="stylesheet" href="../libs/css/app.css?v=7.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 	<script src="../libs/js/main.js?v=1.0"></script>
 	<link rel="stylesheet" href="../libs/css/choices.min.css?v=2.0">
