f97319cc80fabf0c6ccd8eb7fead8c1a914b809c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-23 22:48:44 -0500

    02-23 Workers 10:48

--- workers/lib.php
+++ workers/lib.php
@@ -57,7 +57,7 @@ GROUP BY
     U.nombre;";
     $filter = whe_employee();
     $sql.= $filter['where'];$params = $filter['params'];$types = $filter['types'];
-    $sql.=" GROUP BY U.Departamento,U.nombre";    
+    $sql.=" GROUP BY U.nombre";    
     $result = exec_sql($sql);
     return $result ? $result[0] : ['Total_Reportados' => 0, 'Total_Digitados' => 0];
 }
