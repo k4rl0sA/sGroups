2eb2892e428eb0dc765ee90c6ea72624d4e56553
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-24 21:36:36 -0500

    02-24 Workers 21:36

--- workers/lib.php
+++ workers/lib.php
@@ -47,10 +47,10 @@ function whe_employee() {
     return fil_where($filtros);
 }
 function tot_employee() {
-    /* $sql = "SELECT count(*) AS Total from usuarios where ";
+    $sql = "SELECT count(*) AS Total from usuarios where ";
     $filter = whe_employee();
     $sql.= $filter['where'];$params = $filter['params'];$types = $filter['types'];
-    show_sql($sql,array_merge($params,[]),$types ."ii"); */
+    show_sql($sql,array_merge($params,[]),$types ."ii");
 
 
    /*  $result = exec_sql($sql);
@@ -72,12 +72,12 @@ function tot_employee() {
 
 
 
-    $sql = "SELECT SUM(R.cant_report) AS Total_Reportados 
+   /*  $sql = "SELECT SUM(R.cant_report) AS Total_Reportados 
     FROM `repor_diario` R LEFT JOIN `usuarios` U ON R.usu_create = U.id_usuario where ";
     $filter = whe_employee();
     $sql.= $filter['where'];$params = $filter['params'];$types = $filter['types']; 
     $sql.=" GROUP BY U.nombre";
-    show_sql($sql,array_merge($params,[]),$types ."ii");
+    show_sql($sql,array_merge($params,[]),$types ."ii"); */
     /*$result = exec_sql($sql);
     return $result ? $result[0] : ['Total_Reportados' => 0, 'Total_Digitados' => 0]; */
 }
