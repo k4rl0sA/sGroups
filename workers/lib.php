f1ecd91bb8b5d5e765a4df8bae4eab42311fd513
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-26 17:17:34 -0500

    02-26 Workers 17:17

--- workers/lib.php
+++ workers/lib.php
@@ -208,7 +208,9 @@ $where.=" GROUP BY U.Departamento,U.nombre";
     function opc_arl($id=''){
         return opc_sql('SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=5 and estado="A" ORDER BY 1',$id);
     }
-
+    function opc_estado($id=''){
+        return opc_sql('SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=6 and estado="A" ORDER BY 1',$id);
+    }
 function formato_dato($a,$b,$c,$d){
 	$b=strtolower($b);
 	$rta=$c[$d];
