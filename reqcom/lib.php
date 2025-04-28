853fe4ec025e8bb1f5296c00b95e1ab5fa1dbd97
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-27 20:32:08 -0500

    04-27 requerimientos 20:32

--- reqcom/lib.php
+++ reqcom/lib.php
@@ -147,6 +147,15 @@ function cmp_comreq() {
     if ($d == "") {$d = $t;}
     $o = 'req';
     $c[] = new cmp('id', 'h', 100, $d['id_reqcom'], $w, '', 0, '', '', '', false, '', 'col-1');
+    $c[] = new cmp('act', 's', 10, $d['actividad'], $w.' '.$o, 'Actividad', 'actividad', '', '', true, true, '', 'col-3');
+    $c[] = new cmp('ofic', 't', 80, $d['oficina'], $w.' '.$o, 'Nombre Oficina', 'oficina', '', '', true, true, '', 'col-3');
+    $c[] = new cmp('dir', 't', 50, $d['direccion'], $w.' '.$o, 'Dirección', 'direccion', '', '', true, true, '', 'col-3');
+    $c[] = new cmp('cont', 't', 10, $d['n_contacto'], $w.' '.$o, 'Teléfono Contacto', 'n_contacto', '', '', true, true, '', 'col-2');
+    $c[] = new cmp('dep', 's', 1, $d['departamento'], $w.' '.$o, 'Departamento', 'departamento', '', '', true, true, '', 'col-2');
+    $c[] = new cmp('ciu', 's', 1, $d['ciudad'], $w.' '.$o, 'Ciudad', 'ciudad', '', '', true, true, '', 'col-2');
+    $c[] = new cmp('est', 's', 2, $d['estado'], $w.' '.$o, 'Estado', 'estado', '', '', true, true, '', 'col-2');
+
+
     $c[] = new cmp('act', 's', 3, $d['actividad'], $w.' '.$o, 'Actividad', 'actividad', '', '', true, true, '', 'col-3');
     $c[] = new cmp('cot', 's', 3, $d['cotizacion'], $w.' '.$o, 'Cotización', 'cotizacion', '', '', true, true, '', 'col-3');
     $c[] = new cmp('req', 's', 3, $d['requerimiento'], $w.' '.$o, 'Requerimiento', 'requerimiento', '', '', true, true, '', 'col-4');
