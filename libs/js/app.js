770487c2874d8fa517996d7c82a0d5e0a22af80f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-28 21:46:02 -0500

    04-27 requerimientos 21:45

--- libs/js/app.js
+++ libs/js/app.js
@@ -1,6 +1,25 @@
+/******************CREATE RULES VALIDATION************************/
+function EnabDepeDynamic(classes, conditions) {
+	const allConditionsMet = conditions.every(condition => {
+		const element = document.getElementById(condition.id);
+		if (!element) {
+			console.error(`Elemento con ID ${condition.id} no encontrado.`);
+			return false;
+		}
+		return condition.compare ? element.value === condition.value : element.value !== condition.value;
+	});
+	const selector = classes.map(cls => `select.${cls}, input.${cls}, textarea.${cls}`).join(', ');
+	const elements = document.querySelectorAll(selector);
+	elements.forEach(element => {
+		enaFie(element, !allConditionsMet);
+	});
+}
+/******************CREATE RULES VALIDATION************************/
+/******************VALIDATION ENABLED OR DISABLED REQ COM************************/
 function ActiRequCome(){
 	const conditions = [
 		{ id: 'act', value: '6', compare: true }
 	];
 	EnabDepeDynamic(['aCt'], conditions);
 }
+/******************VALIDATION ENABLED OR DISABLED REQ COM************************/
