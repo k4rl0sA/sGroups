f9363a699b5b4d5ad2b2a13262537d30208fc0c9
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-28 21:19:39 -0500

    04-27 requerimientos 21:19

--- libs/js/main.js
+++ libs/js/main.js
@@ -1710,3 +1710,20 @@ function updCsrfTkn(newToken) {
     }
 }
 /******************CREATE TKNS CSRF************************/
+/******************CREATE RULES VALIDATION************************/
+function EnabDepeDynamic(classes, conditions) {
+	const allConditionsMet = conditions.every(condition => {
+		const element = document.getElementById(condition.id);
+		if (!element) {
+			console.error(`Elemento con ID ${condition.id} no encontrado.`);
+			return false;
+		}
+		return condition.compare ? element.value === condition.value : element.value !== condition.value;
+	});
+	const selector = classes.map(cls => `select.${cls}, input.${cls}, textarea.${cls}`).join(', ');
+	const elements = document.querySelectorAll(selector);
+	elements.forEach(element => {
+		enaFie(element, !allConditionsMet);
+	});
+}
+/******************CREATE RULES VALIDATION************************/
