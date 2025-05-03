b61d9b8d4c61881affd91e6f963684c63008aeef
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2025-05-03 18:44:04 -0500

    20250503_fer

--- reqcom/lib.php
+++ reqcom/lib.php
@@ -257,7 +257,7 @@ function formato_dato($a, $b, $c, $d) {
     $b = strtolower($b);
     $rta = $c[$d];
     if (($a == 'comreq') && ($b == 'acciones')) {
-        $rta = "<nav class='menu right'>";
+        $rta = "<nav class='menu left'>";
         $rta .= "<li class='fa-solid fa-pen-to-square icon' title='Editar Requerimiento' id='".$c['ACCIONES']."' Onclick=\"mostrar('comreq','pro',event,'','lib.php',3,'Requerimientos');\"></li>";
         $rta .= "<li class='fa-solid fa-paper-plane icon' title='Asignar Requerimiento' id='".$c['ACCIONES']."' Onclick=\"mostrar('reqasig','pro',event,'','asigna.php',3,'Asignación de Requerimientos');\"></li>";
         $rta .= "<li class='fa-solid fa-tasks icon' title='Gestionar Requerimiento' id='".$c['ACCIONES']."' Onclick=\"mostrar('comreq','pro',event,'','lib.php',3,'Requerimientos');\"></li>";
