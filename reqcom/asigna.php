60ed8f8acb36c84ab698d745680d0ad95724501a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-03 15:29:30 -0500

    05-03 asignaReq 15:29

--- reqcom/asigna.php
+++ reqcom/asigna.php
@@ -133,6 +133,15 @@ function cmp_reqasig() {
     $o = 'req';
     $c[] = new cmp('id', 'h', 100, $d['id_reqseg'], $w, '', 0, '', '', '', false, '', 'col-1');
     $c[] = new cmp('ofi', 'lb', 0, $d['oficina'] ?? '', 'reqasig', 'Oficina', '', '', '', false, false, '', 'col-6');
+    $c[] = new cmp('act', 'lb',500 , $d['actividad'] ?? '', $w.' '.$o, 'Actividad', 'actividades', '', '', true, true, '', 'col-3','ActiRequCome();');
+    $c[] = new cmp('cot', 'lb', 3, $d['cotizacion']?? '', $w.' '.$v1.' '.$o, 'Cotización', 'cotizaciones', '', '', true, false, '', 'col-3');
+    $c[] = new cmp('req', 'lb', 3, $d['requerimiento']?? '', $w.' '.$v1.' '.$o, 'Requerimiento', 'requerimientos', '', '', true, false, '', 'col-4');
+    $c[] = new cmp('emp', 'lb', 3, $d['cod_empresa']?? '', $w.' '.$o, 'Empresa', 'empresas', '', '', true, true, '', 'col-3');
+    $c[] = new cmp('con', 'lb', 3, $d['cod_contacto']?? '', $w.' '.$o, 'Contacto', 'contactos', '', '', true, true, '', 'col-3');
+    $c[] = new cmp('ofi', 'lb', 3, $d['cod_oficina']?? '', $w.' '.$o, 'Oficina', 'oficinas', '', '', true, true, '', 'col-2');
+    $c[] = new cmp('des', 'lb', 500, $d['descripcion']?? '', $w.' '.$o, 'Descripción', 'descripcion', '', '', true, true, '', 'col-2');
+    $c[] = new cmp('pen', 'lb', 500, $d['pendientes']?? '', $w.' '.$o, 'Pendientes', 'pendientes', '', '', false, true, '', 'col-12');
+
     $c[] = new cmp('req', 's', 3, $d['idreqcom'], $w.' '.$o, 'Requerimiento', 'requerimientos', '', '', true, true, '', 'col-4');
     $c[] = new cmp('per', 's', 3,'', $w.' '.$o, 'Perfil', 'perfil', '', '', true, true, '', 'col-4');
     $c[] = new cmp('asi', 's', 3, $d['asignado'], $w.' '.$o, 'Asignado a', 'usuarios', '', '', true, true, '', 'col-4');
