96a2647afe4334cac7567d3448940e75c5dcf7f0
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-04 17:12:46 -0500

    05-04 asignaReq 17:12

--- reqcom/asigna.php
+++ reqcom/asigna.php
@@ -243,7 +243,7 @@ function opc_perasi(){
         log_error("info: PERASI: " . $info['responseResult']); */
         return json_encode($info['responseResult'] ?? []);
     }
-    return json_encode(['OK']);
+    return json_encode([$_REQUEST]);
 }
 
 function formato_dato($a, $b, $c, $d) {
