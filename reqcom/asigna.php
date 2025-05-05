beff1e64a8f5aed1e0767bf54ed5fe14349f9fd5
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-04 21:11:10 -0500

    05-04 asignaReq 21:11

--- reqcom/asigna.php
+++ reqcom/asigna.php
@@ -155,10 +155,11 @@ function cmp_reqasig() {
 function get_reqasig() {
     $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
     if ($id === '0' || empty($id)) return "";
+    $info = mysql_prepd("SELECT * FROM req_asig WHERE idreqcom = ?", [['type' => 'i', 'value' => $id]]);
     $sql = "SELECT * FROM req_asig WHERE idreqcom = ?";
     $params = [['type' => 'i', 'value' => $id]];
     $types = 'i';
-    show_sql($sql, array_column($params, 'value'), $types);
+    show_sql($sql, $id,'i');
     $info = mysql_prepd($sql, $params);
     if (isset($info['responseResult'][0])) {
         return $info['responseResult'][0];
