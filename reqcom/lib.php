ecb348d434f18ff7cc49acab4d1eeeeb27ff2018
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-31 21:19:26 -0500

    05-31 ReqCom 21:19

--- reqcom/lib.php
+++ reqcom/lib.php
@@ -276,7 +276,7 @@ function formato_dato($a, $b, $c, $d) {
         $rta = "<nav class='menu left'>";
         $rta .= "<li class='fa-solid fa-pen-to-square icon' title='Editar Requerimiento' id='".$c['ACCIONES']."' Onclick=\"mostrar('comreq','pro',event,'','lib.php',3,'Requerimientos');\"></li>";
         $perfil = obtenerPerfil($_SESSION['documento']);
-        if ($perfil == '1') {
+        if ($perfil == '1'|| $perfil == '10' ) {
             $rta .= "<li class='fa-solid fa-paper-plane icon' title='Asignar Requerimiento' id='".$c['ACCIONES']."' Onclick=\"mostrar('reqasig','pro',event,'','asigna.php',3,'Asignación de Requerimientos');\"></li>";
             $rta .= "<li class='fa-solid fa-tasks icon' title='Gestionar Requerimiento' id='".$c['ACCIONES']."' Onclick=\"mostrar('reqlidser','pro',event,'','gestiona.php',3,'Gestión de Requerimientos');\"></li>";
         }
