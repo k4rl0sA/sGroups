09ade6dc113388e3daf5580cf0ee2a988289bedb
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-31 19:45:58 -0500

    05-31 ReqCom 19:45

--- src/gestion.php
+++ src/gestion.php
@@ -202,9 +202,16 @@ function opc_sql($sql, $val = null, $id_column = null, $descripcion_column = nul
       }
       $id = isset($row[$id_column]) ? $row[$id_column] : null;
       $descripcion = isset($row[$descripcion_column]) ? $row[$descripcion_column] : null;
-      if ($id !== null) { // Solo se requiere que el ID no sea null
-          $selected = ($val !== null && strtoupper($id) == strtoupper($val)) ? " selected" : "";
-          $rta .= "<option value='" . $id . "'$selected>" . htmlentities($descripcion ?? $id, ENT_QUOTES) . "</option>"; // Mostrar ID si la descripción es null
+         if ($id !== null) { // Solo se requiere que el ID no sea null
+    $selected = '';
+    if ($val !== null) {
+        if (is_array($val) && in_array(strtoupper($id), array_map('strtoupper', $val))) {
+            $selected = " selected";
+        } elseif (strtoupper($id) == strtoupper($val)) {
+            $selected = " selected";
+        }
+    }
+    $rta .= "<option value='" . $id . "'$selected>" . htmlentities($descripcion ?? $id, ENT_QUOTES) . "</option>";
       } else {
           log_error("opc_sql: Fila con ID NULL en la consulta: " . $sql . ". Fila: " . var_export($row, true));
       }
