3d6681c00dae26d4118faa8694fc385dcdd717ee
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-25 13:44:19 -0500

    08-25 ReqCom 13:44

--- reqasig/asigna.php
+++ reqasig/asigna.php
@@ -130,7 +130,7 @@ function cmp_reqasig() {
     $uPd = $_REQUEST['id'] == '0' ? true : false;
     $d = get_reqasig();
     $r=get_comreq();
-    var_dump($r);
+    // var_dump($r);
     if ($d == "") $d = $t; 
     $o = 'req';
     $up = (!empty($d['asignado'])) ? false  : true ;
